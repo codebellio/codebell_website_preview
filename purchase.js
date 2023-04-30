48a7ba91842fcec28a49d74ad13627016b6e3326
SushainGupta <sushaingupta135@gmail.com>
2023-04-30 16:26:06 +0530

    Test: testing purchase page

--- purchase.js
+++ purchase.js
@@ -8,10 +8,11 @@ console.log(orderObj);
 const UUID =
   Object.keys(orderObj).length !== 0 ? orderObj.Result.Order.UUID : null;
 
+console.log(UUID);
 console.log(url.substring(url.lastIndexOf("?") + 4));
 
 if (url.substring(url.lastIndexOf("?") + 4) != UUID) {
-  window.location.replace("https://preview.codebell.io/");
+  // window.location.replace("https://preview.codebell.io/");
 } else {
   localStorage.clear("customerData");
 }
