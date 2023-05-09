47f0761b62436df4ffc18830fc210f4cf3e30d42
SushainGupta <sushaingupta135@gmail.com>
2023-05-10 01:12:46 +0530

    Minor error fixes

--- order.js
+++ order.js
@@ -37,7 +37,7 @@ const appliedCouponDetails = orderSummaryForm.querySelector(
 );
 
 const checkoutBtn = orderSummaryForm.querySelector(".checkout.button");
-const cartDetailsElem = document.querySelector("#cartDetails");
+const cartDetailsElem = orderSummaryForm.querySelector("#cartDetails");
 
 var url = window.location.href;
 
@@ -522,10 +522,11 @@ function findTotal(discountAmm) {
   const finalAmountElem = orderSummaryForm.querySelector(".finalAmount span");
   finalAmountElem.innerHTML = `₹${finalAmount}`;
 
-  finalAmount === 50 &&
-    (cartDetailsElem.innerHTML = `
+  finalAmount === 50
+    ? (cartDetailsElem.innerHTML = `
   <h5 style="padding: 1em;">No items in cart..</h5>
-  `);
+  `)
+    : (finalAmountElem.innerHTML = `₹${finalAmount}`);
 }
 
 const orderContainerElem = orderSummaryForm.querySelector(".orderContainer");
