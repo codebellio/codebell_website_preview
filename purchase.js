8bbea3c03f5020a9c52debf141561e222ff4d783
SushainGupta <sushaingupta135@gmail.com>
2023-05-15 18:49:58 +0530

    New chnages

--- purchase.js
+++ purchase.js
@@ -1,16 +1,16 @@
-const url = window.location.href;
-orderObj = localStorage.getItem("customerData")
-  ? JSON.parse(localStorage.getItem("customerData"))
-  : {};
+// const url = window.location.href;
+// orderObj = localStorage.getItem("customerData")
+//   ? JSON.parse(localStorage.getItem("customerData"))
+//   : {};
 
-// var UUID = Object.keys(orderObj).length !== 0 ? orderObj.UUID : null;
+// // var UUID = Object.keys(orderObj).length !== 0 ? orderObj.UUID : null;
 
-// localStorage.setItem("orderUUID", JSON.stringify(UUID));
+// // localStorage.setItem("orderUUID", JSON.stringify(UUID));
 
-const UUID = JSON.parse(localStorage.getItem("orderUUID"));
+// const UUID = JSON.parse(localStorage.getItem("orderUUID"));
 
-if (url.substring(url.lastIndexOf("?") + 4) != UUID) {
-  window.location.replace("https://preview.codebell.io/");
-} else {
-  localStorage.removeItem("customerData");
-}
+// if (url.substring(url.lastIndexOf("?") + 4) != UUID) {
+//   window.location.replace("https://preview.codebell.io/");
+// } else {
+//   localStorage.removeItem("customerData");
+// }
