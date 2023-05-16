aab797e45c1b3e7af2c88cc60dcaaf2437f021f9
SushainGupta <sushaingupta135@gmail.com>
2023-05-16 22:48:18 +0530

    Minor changes in product object

--- products.js
+++ products.js
@@ -2,7 +2,8 @@ const productsObj = {
   1: {
     ProductID: 1,
     Title: "Codebell for Home",
-    Photo: "./assets/img/3.5x5.png",
+    Photo:
+      "https://s3.ap-south-1.amazonaws.com/device.codebell.io/products/cproduct1.png",
     Summary:
       "Upon scanning Codebell, a screenshot of visitor is stored in your app. Keep track of all visitors for extra safety.",
     Price: 365,
@@ -14,7 +15,8 @@ const productsObj = {
   2: {
     ProductID: 2,
     Title: "Codebell for your Vehicle",
-    Photo: "./assets/img/cccproduct2.png",
+    Photo:
+      "https://s3.ap-south-1.amazonaws.com/device.codebell.io/products/cccproduct2.png",
     Summary:
       "Get LIVE preview when anyone scans the CodeBell. Talk to them or let them know if you are away.",
     Price: 365,
@@ -27,7 +29,8 @@ const productsObj = {
     Title: "Codebell Office",
     ProductID: 3,
     Keyword: "office",
-    Photo: "./assets/img/v1.png",
+    Photo:
+      "https://s3.ap-south-1.amazonaws.com/device.codebell.io/products/product3.png",
     Summary:
       "Codebell Shop Connect is a QR code-based scanner that allows customers to connect with shop owners even when the shop is closed or the owner is unavailable. This innovative tool ensures that businesses can remain connected with their customers 24/7 without the need for physical presence.",
     Price: 400,
@@ -41,7 +44,6 @@ const productWrapperElem = document.querySelector("#productWrapper");
 if (productWrapperElem) {
   Object.values(productsObj).map((products, index) => {
     // productWrapperElem.innerHTML += `
-                
     // `;
   });
 }
